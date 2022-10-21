a26f6ebd093b656456ba0b649589d1a77518d644
nicole1312 <102580592+nicole1312@users.noreply.github.com>
2022-10-21 20:00:33 +0100

    Added the functionality of the select element.

--- javascript/index.js
+++ javascript/index.js
@@ -1,29 +1,55 @@
 function updateTime(){
-let londonElement=document.querySelector("#london");
-let londonDateElement=londonElement.querySelector(".date");
-let londonTimeElement=londonElement.querySelector(".time");
-let londonTime=moment().tz("Europe/London");
+ let londonElement=document.querySelector("#london");
+  if (londonElement){
+     let londonDateElement=londonElement.querySelector(".date");
+     let londonTimeElement=londonElement.querySelector(".time");
+     let londonTime= moment().tz("Europe/London");
 
-londonDateElement.innerHTML=londonTime.format("dddd, MMMM Do YYYY");
-londonTimeElement.innerHTML=londonTime.format("H:mm:ss [<small>]A[</small>]");
+     londonDateElement.innerHTML=londonTime.format("dddd, MMMM Do YYYY");
+     londonTimeElement.innerHTML=londonTime.format("h:mm:ss [<small>]A[</small>]");
+    }
+ let lisbonElement=document.querySelector("#lisbon");
+  if (lisbonElement){
+     let lisbonDateElement=lisbonElement.querySelector(".date");
+     let lisbonTimeElement=lisbonElement.querySelector(".time");
+     let lisbonTime= moment().tz("Europe/Lisbon");
 
-let lisbonElement=document.querySelector("#lisbon");
-let lisbonDateElement=lisbonElement.querySelector(".date");
-let lisbonTimeElement=lisbonElement.querySelector(".time");
-let lisbonTime=moment().tz("Europe/Lisbon");
+     lisbonDateElement.innerHTML=lisbonTime.format("dddd, MMMM Do YYYY");
+     lisbonTimeElement.innerHTML=lisbonTime.format("h:mm:ss [<small>]A[</small>]");
+    }
 
-lisbonDateElement.innerHTML=lisbonTime.format("dddd, MMMM Do YYYY");
-lisbonTimeElement.innerHTML=lisbonTime.format("H:mm:ss [<small>]A[</small>]");
+ let stockholmElement=document.querySelector("#stockholm");
+   if (stockholmElement){
+      let stockholmDateElement=stockholmElement.querySelector(".date");
+      let stockholmTimeElement=stockholmElement.querySelector(".time");
+      let stockholmTime= moment().tz("Europe/Stockholm");
 
-
-let stockholmElement=document.querySelector("#stockholm");
-let stockholmDateElement=stockholmElement.querySelector(".date");
-let stockholmTimeElement=stockholmElement.querySelector(".time");
-let stockholmTime=moment().tz("Europe/Stockholm");
-
-stockholmDateElement.innerHTML=stockholmTime.format("dddd, MMMM Do YYYY");
-stockholmTimeElement.innerHTML=stockholmTime.format("H:mm:ss [<small>]A[</small>]");
+      stockholmDateElement.innerHTML=stockholmTime.format("dddd, MMMM Do YYYY");
+      stockholmTimeElement.innerHTML=stockholmTime.format("h:mm:ss [<small>]A[</small>]");
+    }
 }
 
-updateTime();
-setInterval(updateTime, 1000);
+
+
+function updateCity(event){
+
+   let cityTimeZone = event.target.value;
+   let cityName=cityTimeZone.replace("_", " ").split("/")[1];
+   let cityTime= moment().tz(cityTimeZone);
+   let citiesElement=document.querySelector("#cities");
+   citiesElement.innerHTML= `
+   <div class="city">
+     <div>
+       <h2> ${cityName} </h2>
+       <div class="date"> ${cityTime.format("dddd, MMMM Do YYYY")}</div>
+     </div> 
+     <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format("A")} </small> </div>
+   </div>
+   `;
+ }
+
+ let citiesSelectElement=document.querySelector("#city");
+ citiesSelectElement.addEventListener("change", updateCity);
+
+ updateTime();
+ setInterval(updateTime, 1000);
